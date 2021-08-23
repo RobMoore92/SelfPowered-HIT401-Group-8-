1466a4a1ed23225ce8df6d0cd8600f65bcf63654
RobMoore92 <s216147@students.cdu.edu.au>
2021-08-23 19:04:37 +0930

    added tailwind and craco / converted to a js project

--- src/components/ExploreContainer.jsx
+++ src/components/ExploreContainer.jsx
@@ -1,11 +1,9 @@
 import './ExploreContainer.css';
 
-interface ContainerProps { }
-
-const ExploreContainer: React.FC<ContainerProps> = () => {
+const ExploreContainer = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
+      <strong className="text-3xl">Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
